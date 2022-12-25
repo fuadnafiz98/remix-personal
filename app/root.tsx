95afccc8452fda5c0151a7547ccbc1d8d94f31c3
fuadnafiz98 <fuadnafiz98@fuadnafiz98s-MacBook-Air.local>
2022-12-25 18:58:25 +0700

    added google search console

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -9,13 +9,12 @@ import {
 } from "@remix-run/react";
 import styles from "./tailwind.css";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "fuadnafiz98",
+  description: "Personal Website of Md. Muhtasim Fuad",
   viewport: "width=device-width,initial-scale=1",
 });
 
@@ -24,6 +23,10 @@ export default function App() {
     <html lang="en">
       <head>
         <Meta />
+        <meta
+          name="google-site-verification"
+          content="ogzTC4YyZQqjtzLFFtkLpFcd2Q8TiMdEu4XLelLyfqY"
+        />
         <Links />
       </head>
       <body>
